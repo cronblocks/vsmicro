c3570d23f16e6f5623312d827c86a44ff4b9560d
usama <mahmoodify@gmail.com>
2022-08-02 01:14:39 +0500

    adding more info about stm

--- src/Libs/VSmicro.Datatypes/Devices/DeviceManufacturer.cs
+++ src/Libs/VSmicro.Datatypes/Devices/DeviceManufacturer.cs
@@ -10,7 +10,7 @@
         NXP,                   // Eindhoven, Netherlands - merged with Freescale Semiconductor
         Renesas,               // Tokyo, Japan
         SiliconLabs,           // Austin, Texas, USA - EFM32™ ARM® Cortex® 32-bit and EFM8™ 8051-based 8-bit
-        STMicroelectronics,    // Plan-les-Ouates, Switzerland
+        STMicroelectronics,    // Plan-les-Ouates, Switzerland - Franco-Italian
         TexasInstruments,      // Dallas, Texas, USA
     }
 }
