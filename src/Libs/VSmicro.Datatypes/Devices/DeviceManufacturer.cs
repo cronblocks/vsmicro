2ecb9b4ebb83ae085ea28f6456cccd27220d94fa
usama <mahmoodify@gmail.com>
2022-08-02 01:19:51 +0500

    assigning numbers to each flag

--- src/Libs/VSmicro.Datatypes/Devices/DeviceManufacturer.cs
+++ src/Libs/VSmicro.Datatypes/Devices/DeviceManufacturer.cs
@@ -5,14 +5,14 @@ namespace VSmicro.Datatypes.Devices
     [Flags]
     public enum DeviceManufacturer
     {
-        NONE,
-        Cypress,               // California, USA
-        Infineon,              // Neubiberg, Germany - by Siemens AG
-        Microchip,             // Chandler, USA - in 2016, Microchip acquired Atmel (AVR and ARM microcontrollers)
-        NXP,                   // Eindhoven, Netherlands - merged with Freescale Semiconductor
-        Renesas,               // Tokyo, Japan
-        SiliconLabs,           // Austin, Texas, USA - EFM32™ ARM® Cortex® 32-bit and EFM8™ 8051-based 8-bit
-        STMicroelectronics,    // Plan-les-Ouates, Switzerland - Franco-Italian
-        TexasInstruments,      // Dallas, Texas, USA
+        NONE = 0,
+        Cypress = 1,                // California, USA
+        Infineon = 2,               // Neubiberg, Germany - by Siemens AG
+        Microchip = 4,              // Chandler, USA - in 2016, Microchip acquired Atmel (AVR and ARM microcontrollers)
+        NXP = 8,                    // Eindhoven, Netherlands - merged with Freescale Semiconductor
+        Renesas = 16,               // Tokyo, Japan
+        SiliconLabs = 32,           // Austin, Texas, USA - EFM32™ ARM® Cortex® 32-bit and EFM8™ 8051-based 8-bit
+        STMicroelectronics = 64,    // Plan-les-Ouates, Switzerland - Franco-Italian
+        TexasInstruments = 128,     // Dallas, Texas, USA
     }
 }
