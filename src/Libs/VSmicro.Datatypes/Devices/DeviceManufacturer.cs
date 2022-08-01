5ae3f1134ab923bc110436f772253eae61f7a057
usama <mahmoodify@gmail.com>
2022-08-02 01:04:09 +0500

    adding more info about siliconlabs

--- src/Libs/VSmicro.Datatypes/Devices/DeviceManufacturer.cs
+++ src/Libs/VSmicro.Datatypes/Devices/DeviceManufacturer.cs
@@ -6,7 +6,7 @@
         Microchip,             // Chandler, USA - in 2016, Microchip acquired Atmel (AVR and ARM microcontrollers)
         NXP,                   // Eindhoven, Netherlands - merged with Freescale Semiconductor
         Renesas,               // Tokyo, Japan
-        SiliconLabs,           // Austin, Texas, USA
+        SiliconLabs,           // Austin, Texas, USA - EFM32™ ARM® Cortex® 32-bit and EFM8™ 8051-based 8-bit
         STMicroelectronics
     }
 }
