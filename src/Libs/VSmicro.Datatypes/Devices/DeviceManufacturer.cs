2e479badb127084aac6459d978bfd0accd32dbbd
usama <mahmoodify@gmail.com>
2022-08-02 00:58:31 +0500

    adding help for microchip

--- src/Libs/VSmicro.Datatypes/Devices/DeviceManufacturer.cs
+++ src/Libs/VSmicro.Datatypes/Devices/DeviceManufacturer.cs
@@ -3,7 +3,7 @@
     public enum DeviceManufacturer
     {
         None,
-        Microchip,
+        Microchip,             // Chandler, USA
         NXP,                   // Eindhoven, Netherlands - merged with Freescale Semiconductor
         Renesas,               // Tokyo, Japan
         STMicroelectronics
