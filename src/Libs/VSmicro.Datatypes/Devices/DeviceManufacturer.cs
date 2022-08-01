19224a161e59dc595748d170da9f5edd01dc181a
usama <mahmoodify@gmail.com>
2022-08-02 00:57:50 +0500

    adding Microchip

--- src/Libs/VSmicro.Datatypes/Devices/DeviceManufacturer.cs
+++ src/Libs/VSmicro.Datatypes/Devices/DeviceManufacturer.cs
@@ -3,6 +3,7 @@
     public enum DeviceManufacturer
     {
         None,
+        Microchip,
         NXP,                   // Eindhoven, Netherlands - merged with Freescale Semiconductor
         Renesas,               // Tokyo, Japan
         STMicroelectronics
