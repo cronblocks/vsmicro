d399a4febba8af0226ab84136cfdfd017269ffa3
usama <mahmoodify@gmail.com>
2022-08-02 01:05:59 +0500

    adding info about infineon

--- src/Libs/VSmicro.Datatypes/Devices/DeviceManufacturer.cs
+++ src/Libs/VSmicro.Datatypes/Devices/DeviceManufacturer.cs
@@ -3,7 +3,7 @@
     public enum DeviceManufacturer
     {
         None,
-        Infineon,
+        Infineon,              // by Siemens AG
         Microchip,             // Chandler, USA - in 2016, Microchip acquired Atmel (AVR and ARM microcontrollers)
         NXP,                   // Eindhoven, Netherlands - merged with Freescale Semiconductor
         Renesas,               // Tokyo, Japan
